71964ca0cfacb9b12d90ad6b87c874acbcafa840
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-03-02 20:23:07 -0300

    Initial commit

---
+++ Fatec_manhã/src/Fatec.java
@@ -0,0 +1,16 @@
+
+public class Fatec {
+	//atributos
+	String unidade;
+	String curso;
+	
+	//métodos
+	public void Retorna_unidade(String u) {
+		System.out.println("Unidade da Fatec:\t" + u);
+	}
+	
+	public void Retora_curso(String c) {
+		System.out.println("Curos da Fatec:\t" + c);
+	}
+
+}
