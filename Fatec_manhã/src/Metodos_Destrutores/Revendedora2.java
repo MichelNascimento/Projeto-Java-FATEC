134bff5899e269ce69a0f60128ff3db71f41d647
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-03-20 11:06:43 -0300

    .

---
+++ Fatec_manhã/src/Metodos_Destrutores/Revendedora2.java
@@ -0,0 +1,15 @@
+package Metodos_Destrutores;
+
+public class Revendedora2 {
+	
+	public static void main(String args[]) {
+		Automovel_II c = new Automovel_II(200, "Ford", 5000.00);
+		
+		c.mostracarro();
+		
+		c = null;
+		System.gc();
+		c.mostracarro();
+	}
+
+}
